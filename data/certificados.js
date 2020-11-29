d5728f418d85ddb1e471304bd1f2cd5f295488e7
Wilder León <wilder_eduardo85@hotmail.com>
2020-11-29 16:44:27 -0300

    First Commit

---
+++ data/certificados.js
@@ -0,0 +1,40 @@
+export const certificados = [
+    {
+        "nombre": "Front-End Certificate",
+        "imagen": "m-front.jpg",
+        "descripción": "Issued by Next-U, this certificate validates all the fundamentals and advanced methods of the 3 basic Front-End branches, such as HTML5, CSS3 and Javascript. In CSS3, methods such as handling the box modeling system and Grid System, Flexbox, Keyframes, animations, transitions and Responsive Web Platform Design with Media Queries are analyzed. In Javascript you can see the fundamentals and also, advanced functions with cycles, iterations, Time Out and Interval functions, methods to consume REST API both as in Vanilla Javascript and in its JQuery library, the methods for manipulating the DOM with event handling.",
+        "url": "https://www.credential.net/330513ab-70d7-4c2b-9e70-1b7c1e3696fc#gs.jhls2t",
+        "emisor": "Next-U"
+    },
+    {
+        "nombre": "Databases",
+        "imagen": "m-base.jpg",
+        "descripción": "Issued by Next-U, this certificate validates the foundations and advanced management of both relational and non-relational databases, the methods to relate the different types of data and how to build tables for a more efficient management of the data are analyzed. The interaction is seen, both in the console and the visual interaction software, in terms of the Relational Bases; the SQL language and its advanced queries are thoroughly investigated, the evaluations are mainly based on MySQL and PostgresQL, which are the best known. As for the Non-Relational, the evaluations are basically focused on Mongo DB and CouchDB.",
+        "url": "https://www.credential.net/2e5f08c0-578c-4259-b83c-e3aeff88678f#gs.jhln8t",
+        "emisor": "Next-U"
+    },
+    {
+        "nombre": "Node",
+        "imagen": "m-node.jpg",
+        "descripción": "Issued by Next-U, this certificate validates the fundamentals to work with Node JS, its most popular library that is Express, as well as its various methods to build all the Backend and design of Web applications, connection to databases, working with sessions, submission of forms through the different methods (both POST and GET), everything that is needed to handle the NPM environment and all its dependencies, working with template engines. The final evaluation consists of building a Blog where all the advanced methods of the express library are applied and working with Multipart. ",
+        "url": "https://learn.nextu.com/badges/badge.php?hash=8867f9f55806c3ebebdaae2b58286938bfdabfa2",
+        "emisor": "Next-U"
+    },
+    {
+        "nombre": "PHP and MySQL",
+        "imagen": "m-php.jpg",
+        "descripción": "Issued by Udemy, this certificate validates the fundamentals of PHP, working with a local server such as Xampp, a deep investigation is made of all the functions and methods available in PHP in its version No. 7, here you learn to build sites and Web Applications using the entire PHP Backend, its connection to MySQL databases, form validation, working with sessions and login, inserting the backend into HTML documents, building Web sites applying secure methods to prevent injection of external code, structuring and design using requires, pagination, multimedia content, among others. ",
+        "url": "https://www.udemy.com/certificate/UC-9b626544-d3f5-4019-a7a3-4d04328166d5/",
+        "emisor": "Udemy"  
+    },
+    {
+        "nombre": "Profesional Web Design",
+        "imagen": "m-diseno.jpg",
+        "descripción": "Issued by Udemy, this certificate validates the fundamentals to make a Website Layout in such a way that advanced Design tools are applied, such as Adobe XD to edit images, apply layers and effects, although this course focuses on design as well the main basic branches of the Front-End are learned, such as HTML, CSS3 and Javascript. JQUery's advanced methods for making great transitions by combining it with CSS are also discussed. You also learn to work with one of the most popular CSS Frameworks such as Bootstrap (well focused on creating Responsive Web sites), CSS pre-processors such as Sass are also analyzed, which generates a super efficient code construction. At the same time, it also delves into how to work with WordPress and the creation of themes with this tool.",
+        "url": "https://www.udemy.com/certificate/UC-188e5761-6d24-418d-b7c8-b1521d23b20b/",
+        "emisor": "Udemy"
+    }
+]
+
+
+
---
+++ data/proyectos.js
@@ -0,0 +1,61 @@
+export const proyectos = [
+    {
+        "nombre": "Marvel App Heroes",
+        "descripcion": "Application that consumes the popular Marvel Developer Web Site API, results of the main characteristics of Heroes and Comics available are shown, here methods are used to encrypt the private keys of the API through hashes generated with MD5 Generator, libraries used: Bootstrap for the styles, Axios for getting results from the API, JQuery for building Javascript logic and Fontawesome for icons. ",
+        "imagen": "m-marvel.jpg",
+        "imagen2": "m-marvel2.jpg",
+        "imagen3": "m-marvel3.jpg",
+        "repositorio" : "https://github.com/Wildereduardoleon85/Marvel-App",
+        "demo_url": "https://wildereduardoleon85.github.io/Marvel-App/",
+        "lenguajes": "Jquery, Bootstrap, Axios." 
+    },
+    {
+        "nombre": "Restaurant",
+        "descripcion": "Web application where a page for a Gourmet Burger Restaurant is displayed, different types of Carousels are displayed, made with Javascript and CSS, scaling hover effects, dynamic menu, carousel that moves only with time intervals, among others. Made with Sass, Vanilla Javascript and CSS3. ",
+        "imagen": "m-restaurant.jpg",
+        "imagen2": "m-restaurant2.jpg",
+        "imagen3": "m-restaurant3.jpg",
+        "repositorio" : "https://github.com/Wildereduardoleon85/Restaurant",
+        "demo_url": "https://wildereduardoleon85.github.io/Restaurant/",
+        "lenguajes": "HTML5, Sass, Vanilla JS." 
+    },
+    {
+        "nombre": "Portfolio",
+        "descripcion": "Application that shows a Template of a Portfolio-type website, here basically CSS3 Design methods are shown, such as the use of CSS Grid, Flexbox, transitions, modification of images with Adobe XD through the application of layers, construction of forms, buttons Ghost type, media queries for responsive design, among others. ",
+        "imagen": "m-portafolio.jpg",
+        "imagen2": "m-portafolio2.jpg",
+        "imagen3": "m-portafolio3.jpg",
+        "repositorio" : "https://github.com/Wildereduardoleon85/Portafolio-CSS-Responsive",
+        "demo_url": "https://wildereduardoleon85.github.io/Portafolio-CSS-Responsive/",
+        "lenguajes": "HTML5, CSS3."
+    },
+    {
+        "nombre": "Econ. indicators",
+        "descripcion": "Web application built to show the economic indicators of Chile, the main feature of this application is that you can see a function that runs automatically every 3 seconds, it was created using the Javascript functions Set Interval () and Set Time Out (), You can also see methods such as Get JSON to obtain public API and a particular function that uses regular expressions to format the string in the way that the values of each currency are displayed, such as the point separator for the units of thousand and comma for decimals. Built with Jquery and pure CSS3 ",
+        "imagen": "m-indicadores.jpg",
+        "imagen2": "m-indicadores2.jpg",
+        "imagen3": "m-indicadores3.jpg",
+        "repositorio" : "https://github.com/Wildereduardoleon85/Indicadores-Econ-micos-Chile-API",
+        "demo_url": "https://wildereduardoleon85.github.io/Indicadores-Econ-micos-Chile-API/",
+        "lenguajes": "HTML5, Jquery, CSS3." 
+    },
+    {
+        "nombre": "Gift Expert App",
+        "descripcion": "Web application built with React where a public API is consumed, results are obtained and a certain number of items are displayed, this App is part of an exercise in an online course, React methods are applied such as the use of hooks, fetch, cycles, conditionals, destructuring of objects, among others. ",
+        "imagen": "m-giftApp.jpg",
+        "imagen2": "m-giftApp2.jpg",
+        "imagen3": "m-giftApp3.jpg",
+        "repositorio" : "https://github.com/Wildereduardoleon85/React---Gift-App",
+        "demo_url": "https://wildereduardoleon85.github.io/React---Gift-App/",
+        "lenguajes": "React Js, CSS3." 
+    },
+    {
+        "nombre": "Workflow",
+        "descripcion": "Web application built with PHP and MySQL, where an identification number is entered and a progress result is shown according to entered data, here methods to connect to a database with the PDO method of PHP, foreach cycles, functions for validation are demonstrated , JQuery was used to change the CSS classes of the Workflow states. ",
+        "imagen": "m-workflow.jpg",
+        "imagen2": "m-workflow2.jpg",
+        "imagen3": "m-workflow3.jpg",
+        "repositorio" : "https://github.com/Wildereduardoleon85/Workflow",
+        "lenguajes": "PHP, MySQL, Jquery, CSS3."
+    }
+]
